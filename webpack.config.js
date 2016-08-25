4f99617fef62df2f009e84da9edb5ae2f264261e
Jesse Kramer <jesse.kramer@liquidpineapple.net>
2016-08-26 00:50:30 +0200

    added webpack build script

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: __dirname + "/app/js/src/app.js",
+  output: {
+    path: __dirname + "/app/js",
+    filename: "app.min.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ]
+};

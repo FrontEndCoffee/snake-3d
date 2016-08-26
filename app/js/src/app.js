d99f4ebd3942c1cbde0e478ed4edcda272f56ad5
Jesse Kramer <jesse.kramer@liquidpineapple.net>
2016-08-26 15:35:08 +0200

    done refactoring, got 3d working

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -1,152 +1,41 @@
 'use strict'
 
-const PERSPECTIVE_CONSTANT = 0.875
-
-const shapes = require('./shapes')
-const Vector = shapes.Vector
-const Line = shapes.Line
-const Polygon = shapes.Polygon
-const Entity = shapes.Entity
+import { Vector, Entity, Rect } from './geometry'
 
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 
-const canvasSize = new Vector(canvas.clientWidth, canvas.clientHeight)
+context.translate(canvas.clientWidth/2, canvas.clientHeight/2)
+context.lineWidth = 2
 
-const blockSize = new Vector(150, 200)
+new Rect(100, 100, -150, -50).stroke3d(context, 'dodgerblue', 'darkblue')
+new Rect(100, 100,  150,   0).fill3d(context, 'orangered', 'darkred')
+new Rect(100, 100, -300, 100).fill3d(context, 'dodgerblue', 'darkblue')
+new Rect(100, 100,  300, 100).stroke3d(context, 'orangered', 'darkred')
 
+const hexagon = new Entity(
+  [
+    new Vector(-35,50), new Vector( 35,50),
+    new Vector(60,0), new Vector(35,-50),
+    new Vector(-35,-50), new Vector(-60,0)
+  ],
+  new Vector(100,150)
+)
+hexagon.stroke3d(context, 'lime', 'green')
 
-const position = new Vector(0,0)
-const blockShape = new Polygon([
-  new Vector( 64,  64),
-  new Vector( 64, -64),
-  new Vector(-64, -64),
-  new Vector(-64,  64)
-])
+const star = new Entity(
+  [
+    new Vector(0,-120), new Vector(20,-40),
+    new Vector(100,-40), new Vector(40,20),
+    new Vector(60,100), new Vector(0,60),
+    new Vector(-60,100), new Vector(-40,20),
+    new Vector(-100,-40), new Vector(-20,-40)
+  ],
+  new Vector(-100, 150)
+)
+star.fill3d(context, 'magenta', 'purple')
 
-const blockEntity = new Entity(blockShape, position)
-blockEntity.position = new Vector(12, 23)
-
-console.log('sourcemap test')
-
-// init
-context.translate(canvasSize.x/2, canvasSize.y/2)
-context.fillStyle = "rgba(255,255,255,0.9)"
-window.ctx = context
-
-window.onmousemove = function(e) {
-
-  const mousePos = new Vector(
-    e.clientX - canvas.offsetLeft,
-    e.clientY - canvas.offsetTop
-  )
-
-  clearFrame()
-  drawCursor(mousePos)
-
-  blockEntity.position = mousePos
-
-  // create shadow Entity
-  const line = new Line(
-    blockEntity.position,
-    new Vector(0,0)
-  )
-
-  const blockShadowEntity = new Entity(
-    blockEntity.verticies,
-    blockEntity.position
-  )
-
-  const frontpaneVertecies = blockEntity.getVerticies()
-  const backpaneVertecies = frontpaneVertecies.map(addPerspective)
-
-  context.fillStyle = 'dodgerblue'
-  fillPoly([
-    new Vector(-10,-15),
-    new Vector( 10,-15),
-    new Vector( 15,  0),
-    new Vector( 10, 15),
-    new Vector(-10, 15),
-    new Vector(-15,  0)
-  ])
-
-  // draw backpane dots (#777)
-  context.fillStyle = '#777'
-  backpaneVertecies.forEach(function(vertexVector) {
-    context.fillRect(vertexVector.x-1, vertexVector.y-1, 3, 3)
-  })
-
-  // draw backpane lines (#777)
-  context.strokeStyle = '#777'
-  backpaneVertecies.forEach(function(vertexVector, i, vertecies) {
-    const nextVertex = vertecies[(i + 1) % vertecies.length]
-    drawLine(vertexVector, nextVertex)
-  })
-
-  // draw middle lines (#bbb)
-  context.strokeStyle = '#bbb'
-  backpaneVertecies.forEach(function(vertexVector, i) {
-    const frontpaneVertex = frontpaneVertecies[i]
-    drawLine(vertexVector, frontpaneVertex)
-  })
-
-
-
-  // draw frontpane lines (#fff)
-  context.strokeStyle = '#fff'
-  frontpaneVertecies.forEach(function(vertexVector, i, vertecies) {
-    const nextVertex = vertecies[(i + 1) % vertecies.length]
-    drawLine(vertexVector, nextVertex)
-  })
-
-  // draw frontpane dots (#fff)
-  context.fillStyle = '#fff'
-  frontpaneVertecies.forEach(function(vertexVector) {
-    context.fillRect(vertexVector.x-1, vertexVector.y-1, 3, 3)
-  })
-
-
-}
-
-function clearFrame() {
-  context.clearRect(
-    -canvasSize.x/2,
-    -canvasSize.y/2,
-    canvasSize.x,
-    canvasSize.y
-  )
-}
-
-function fillPoly(vertecies) {
-  context.beginPath()
-  vertecies.forEach((v, i) => {
-    if (i === 0) {
-      context.moveTo(v.x, v.y)
-    } else {
-      context.lineTo(v.x, v.y)
-    }
-  })
-  context.fill()
-}
-
-function drawLine(vector1, vector2) {
-  context.beginPath()
-  context.moveTo(vector1.x, vector1.y)
-  context.lineTo(vector2.x, vector2.y)
-  context.stroke()
-}
-
-function addPerspective(vertexVector) {
-  return new Vector(
-    vertexVector.x * PERSPECTIVE_CONSTANT,
-    vertexVector.y * PERSPECTIVE_CONSTANT
-  )
-}
-
-function drawCursor(mousePosVec) {
-  const oldStyle = context.fillStyle
-  context.fillStyle = '#fff'
-  context.fillRect(mousePosVec.x-5, mousePosVec.y-1, 10, 2)
-  context.fillRect(mousePosVec.x-1, mousePosVec.y-5, 2, 10)
-  context.fillStyle = oldStyle
-}
+const trianglePath = [new Vector(0,-20), new Vector(30,20), new Vector(-30,20)]
+new Entity(trianglePath, new Vector(70, -140)).fill3d(context, 'yellow', 'orange')
+new Entity(trianglePath, new Vector(100, -100)).fill3d(context, 'yellow', 'orange')
+new Entity(trianglePath, new Vector(40, -100)).fill3d(context, 'yellow', 'orange')
--- app/js/src/geometry.js
+++ app/js/src/geometry.js
@@ -18,14 +18,14 @@ export class Line {
     this.v1 = v1
     this.v2 = v2
   }
-  draw(context, color) {
+  stroke(context, color) {
     const oldColor = context.strokeStyle
     context.strokeStyle = color
     context.beginPath()
-    context.moveTo(v1.x, v1.y)
-    context.lineTo(v2.x, v2.y)
+    context.moveTo(this.v1.x, this.v1.y)
+    context.lineTo(this.v2.x, this.v2.y)
     context.stroke()
-    context.strokeStyle = oldStyle
+    context.strokeStyle = oldColor
   }
   get dX() {
     return this.v2.x - this.v1.x
@@ -41,26 +41,99 @@ export class Line {
   }
 }
 
+export const PERSPECTIVE_CONSTANT = 0.875
 export class Entity {
-  constructor(vertexVectors, posVector) {
+
+  constructor(vertecies, posVector) {
     this.position = posVector
-    this.vertexVectors = vertexVectors
+    this.vertecies = vertecies
   }
-  get vectorVectors() {
-    return this.vertexVectors.map(vector => vector.add(this.position))
+  get absVertecies() {
+    return this.vertecies.map(vector => vector.add(this.position))
   }
-  get translate(deltaPos) {
-    this.position = position.add(deltaPos)
+  get perspectiveVertecies() {
+    return this.absVertecies.map(vertex => new Vector(
+      vertex.x * PERSPECTIVE_CONSTANT,
+      vertex.y * PERSPECTIVE_CONSTANT
+    ))
+  }
+  translate(x,y) {
+    this.position = this.position.add(new Vector(x,y))
+  }
+  fill(context, color) {
+    const oldStyle = context.fillStyle
+    context.fillStyle = color
+    context.beginPath()
+    this.absVertecies.forEach((v, i) => {
+      if (i === 0) {
+        context.moveTo(v.x, v.y)
+      } else {
+        context.lineTo(v.x, v.y)
+      }
+    })
+    context.fill()
+    context.fillStyle = oldStyle
+  }
+  stroke(context, color) {
+    strokePoly(this.absVertecies, context, color)
+  }
+  stroke3d(context, fgColor, bgColor) {
+    const oldStyle = context.strokeStyle
+    strokePoly(this.perspectiveVertecies, context, bgColor)
+    this.absVertecies.forEach((vertex, i) => {
+      const vertex2 = this.perspectiveVertecies[i]
+      new Line(vertex, vertex2).stroke(context, bgColor)
+    })
+    strokePoly(this.absVertecies, context, fgColor)
+  }
+  fill3d(context, fgColor, bgColor) {
+    const entity = this
+    this.perspectiveVertecies.forEach(function(currBackVertex, i, vertecies) {
+      const nextBackVertex = vertecies[(i + 1) % vertecies.length]
+      const currFrontVertex = entity.absVertecies[i]
+      const nextFrontVertex = entity.absVertecies[(i + 1) % entity.absVertecies.length]
+      fillPoly([
+        currFrontVertex,
+        nextFrontVertex,
+        nextBackVertex,
+        currBackVertex
+      ], context, bgColor)
+    })
+    fillPoly(entity.absVertecies, context, fgColor)
   }
 }
 
 export class Rect extends Entity {
-  constructor(size = 100, position = new Vector(0,0)) {
+  constructor(width = 100, height = 100, xPos = 0, yPos = 0) {
     super([
-      new Vector( size/2, -size/2),
-      new Vector( size/2,  size/2),
-      new Vector(-size/2,  size/2),
-      new Vector(-size/2, -size/2)
-    ], position)
+      new Vector( width/2, -height/2),
+      new Vector( width/2,  height/2),
+      new Vector(-width/2,  height/2),
+      new Vector(-width/2, -height/2)
+    ], new Vector(xPos, yPos))
   }
+}
+
+// functions
+
+function strokePoly(polygon, context, color) {
+  polygon.forEach((curr, i, vertList) => {
+    const next = vertList[ (i + 1) % vertList.length ]
+    new Line(curr, next).stroke(context, color)
+  })
+}
+
+function fillPoly(polygon, context, color) {
+  const oldColor = context.fillStyle
+  context.fillStyle = color
+  context.beginPath()
+  polygon.forEach((v, i) => {
+    if (i === 0) {
+      context.moveTo(v.x, v.y)
+    } else {
+      context.lineTo(v.x, v.y)
+    }
+  })
+  context.fill()
+  context.fillStyle = oldColor
 }
